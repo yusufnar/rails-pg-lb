a3a521b0e58ca6141a1132ec98aa063cee688cae
Yusuf Nar <yusufnar@gmail.com>
2026-02-11 18:41:19 +0300

    only reads go to the database load balancer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,17 @@ class ApplicationController < ActionController::Base
   private
 
   def switch_database(&block)
-    role = DatabaseLoadBalancer.instance.reading_role
-    ApplicationRecord.connected_to(role: role) do
+    if request.get? || request.head?
+      role = DatabaseLoadBalancer.instance.reading_role
+      ApplicationRecord.connected_to(role: role) do
+        yield
+      end
+      routing_duration = Thread.current[:redis_routing_duration]
+      Rails.logger.info "Redis Routing Duration: #{(routing_duration * 1000).round(2)}ms" if routing_duration
+    else
+      # Write operations (POST, PUT, DELETE, PATCH) directly use primary
       yield
     end
-    routing_duration = Thread.current[:redis_routing_duration]
-    Rails.logger.info "Redis Routing Duration: #{(routing_duration * 1000).round(2)}ms" if routing_duration
   end
 
   # Changes to the importmap will invalidate the etag for HTML responses
